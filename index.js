4a7d46231569e32eaf14eeff246722fdf6af1d5b
suyalamritanshu <amritanshu.suyall2@gmail.com>
2021-09-21 21:58:18 +0530

    Upload functionality added

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
 const PORT = process.env.PORT || 5000;
+const multer = require("multer");
+const path = require("path");
 
 
 dotenv.config();
@@ -16,6 +18,8 @@ dotenv.config();
 
 DbConnect();
 
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 
 
 
@@ -23,6 +27,25 @@ DbConnect();
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, req.body.name);
+    },
+  });
+
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully.");
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 
 app.use("/api/users", userRoute);
