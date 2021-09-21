0e5adca15cb23c6fc796f9d3b0225e419c972bbd
suyalamritanshu <amritanshu.suyall2@gmail.com>
2021-09-21 22:30:55 +0530

    Fixes1

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const express = require("express");
-const router = express.Router();
 const app = express();
 const dotenv = require("dotenv");
+const DbConnect = require('./database');
 const helmet = require("helmet");
 const morgan = require("morgan");
-const multer = require("multer");
 const userRoute = require("./routes/users");
 const authRoute = require("./routes/auth");
 const postRoute = require("./routes/posts");
+const PORT = process.env.PORT || 5000;
+const multer = require("multer");
 const path = require("path");
+
 
 dotenv.config();
 
@@ -17,6 +19,8 @@ dotenv.config();
 DbConnect();
 
 app.use("/images", express.static(path.join(__dirname, "public/images")));
+
+
 
 
 //middleware
